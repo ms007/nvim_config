dfbdc06d47407fa6dea9dee844b9a5aa908c898c
Marc Seiferle <ms007@gmx.ch>
2023-12-10 17:38:43 +0100

    add comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,12 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<Space>q", ":q<cr>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<cr>", opts)
 keymap("n", "<S-h>", ":bprevious<cr>", opts)
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-k>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-k>", "gbc", { noremap = false })
