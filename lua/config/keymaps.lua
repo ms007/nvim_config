1acecb788333311194ee56e926dfe1e9350d54ae
Marc Seiferle <marc.seiferle@bbcag.ch>
2023-12-11 16:22:58 +0100

    add indent-blankline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,12 +3,16 @@ local opts = { noremap = true, silent = true}
 
 keymap("", "<Space>", "<Nop>", opts)
 
+-- Insert
+keymap("i", "jk", "<Esc>", opts)
+
 -- Normal --
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<Space>q", ":q<cr>", opts)
+keymap("n", "<Space>w", ":w<cr>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<cr>", opts)
