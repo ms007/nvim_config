ee2be503eac8fdc1ab4e079ab37c116eb3477d3b
Marc Seiferle <marc.seiferle@bbcag.ch>
2023-12-11 21:10:04 +0100

    customize nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,5 @@
+local icons = require("config.icons")
+
 return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
@@ -6,6 +8,19 @@ return {
   },
   config = function()
     require("nvim-tree").setup({
+      auto_reload_on_write = false,
+      disable_netrw = false,
+      hijack_cursor = false,
+      hijack_netrw = true,
+      hijack_unnamed_buffer_when_opening = false,
+      sort_by = "name",
+      root_dirs = {},
+      prefer_startup_root = false,
+      sync_root_with_cwd = true,
+      reload_on_bufenter = false,
+      respect_buf_cwd = false,
+      on_attach = "default",
+      select_prompts = false,
       actions = {
         open_file = {
           quit_on_open = true
@@ -17,27 +32,114 @@ return {
         custom = { "node_modules", ".git" },
       },
       view = {
-        adaptive_size = true,
-        side = "right"
+        adaptive_size = false,
+        centralize_selection = true,
+        width = 30,
+        side = "right",
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "no",
+        float = {
+          enable = false,
+          quit_on_focus_loss = true,
+          open_win_config = {
+            relative = "editor",
+            border = "rounded",
+            width = 30,
+            height = 30,
+            row = 1,
+            col = 1,
+          },
+        },
       },
       git = {
         enable = true,
         ignore = true,
       },
       renderer = {
+        add_trailing = false,
+        group_empty = false,
         highlight_git = true,
+        full_name = false,
         highlight_opened_files = "none",
-        root_folder_modifier = ":t",
-        icons = {
-          git_placement = "after",
-          glyphs = {
-            folder = {
-              default = "",
-              open = "",
-            }
+        root_folder_label = ":t",
+        indent_width = 2,
+        indent_markers = {
+          enable = false,
+          inline_arrows = true,
+          icons = {
+            corner = "└",
+            edge = "│",
+            item = "│",
+            none = " ",
           },
-        }
-      }
+        },
+        icons = {
+          webdev_colors = use_icons,
+          git_placement = "before",
+          padding = " ",
+          symlink_arrow = " ➛ ",
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+          glyphs = {
+            default = icons.ui.Text,
+            symlink = icons.ui.FileSymlink,
+            bookmark = icons.ui.BookMark,
+            folder = {
+              arrow_closed = icons.ui.TriangleShortArrowRight,
+              arrow_open = icons.ui.TriangleShortArrowDown,
+              default = icons.ui.Folder,
+              open = icons.ui.FolderOpen,
+              empty = icons.ui.EmptyFolder,
+              empty_open = icons.ui.EmptyFolderOpen,
+              symlink = icons.ui.FolderSymlink,
+              symlink_open = icons.ui.FolderOpen,
+            },
+            git = {
+              unstaged = icons.git.FileUnstaged,
+              staged = icons.git.FileStaged,
+              unmerged = icons.git.FileUnmerged,
+              renamed = icons.git.FileRenamed,
+              untracked = icons.git.FileUntracked,
+              deleted = icons.git.FileDeleted,
+              ignored = icons.git.FileIgnored,
+            },
+          },
+        },
+        special_files = { "README.md", "readme.md" },
+        symlink_destination = true,
+      },
+      hijack_directories = {
+        enable = false,
+        auto_open = true,
+      },
+      update_focused_file = {
+        enable = true,
+        debounce_delay = 15,
+        update_root = true,
+        ignore_list = {},
+      },
+      diagnostics = {
+        enable = use_icons,
+        show_on_dirs = false,
+        show_on_open_dirs = true,
+        debounce_delay = 50,
+        severity = {
+          min = vim.diagnostic.severity.HINT,
+          max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+          hint = icons.diagnostics.BoldHint,
+          info = icons.diagnostics.BoldInformation,
+          warning = icons.diagnostics.BoldWarning,
+          error = icons.diagnostics.BoldError,
+        },
+      }, 
     })
   end
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,8 +1,20 @@
+local config = function()
+  require("tokyonight").setup({
+    on_highlights = function(hl, colors)
+      hl.NvimTreeFolderIcon = {
+        fg = "#FCC76A"
+      }
+    end
+      
+  })
+	vim.cmd('colorscheme tokyonight-night')
+end
+
 return {
 	"folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-	config = function()
-		vim.cmd('colorscheme tokyonight-night')
-	end
+	config = config
 }
+
+
